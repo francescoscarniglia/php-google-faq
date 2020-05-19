9ff53ef43900182bf209bdf68ef0494e8bb2cae0
Francesco Scarniglia <francescoscarniglia@gmail.com>
2020-05-19 14:21:39 +0200

    start ex.

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="dist/css/main.css">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>php google faq | Francesco Scarniglia</title>
+</head>
+<body>
+  <!-- Descrizione
+  Riscrivere questa pagina del sito google https://policies.google.com/faq
+  Ci sono diverse domande con relative risposte. Gestire il “Database” (array) e la visualizzazione di queste domande e risposte con PHP.
+  Non preoccupatevi dei link presenti nel testo delle risposte -->
+
+file is ok
+
+
+
+
+  <script src='dist/js/main.js'>
+  </script>
+</body>
+</html>
